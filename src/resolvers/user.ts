5b2995af045fd5a00da976b5d5f9186387f9fe70
Caleb Gates <caleb.gates@henderex.com>
2020-11-22 15:17:49 -0500

    working login with 2 error handlers, next -> change register and break out into multiple files

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import {Resolver, Query, Field, InputType, Mutation, Arg, Ctx} from "type-graphql";
+import {Resolver, Query, Field, InputType, Mutation, Arg, Ctx, ObjectType} from "type-graphql";
 import {MyContext} from "../types";
 import argon2 from "argon2"
 import {User} from "../entities/User";
@@ -11,6 +11,23 @@ class UsernamePasswordInput {
     password:string
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse { //Either errors or user can be returned, in 1 class and optional
+    @Field(()=> [FieldError], {nullable:true})
+    errors?: FieldError[]
+
+    @Field(()=> User, {nullable:true})
+    user?:User
+}
+
 @Resolver()
 export class UserResolver {
     @Mutation(() => User)
@@ -20,10 +37,38 @@ export class UserResolver {
     ){
         const hashedPassword = await argon2.hash(options.password)
         const user = em.create(User, {
-            username: options.username,
+            username: options.username.toLowerCase(),
             password: hashedPassword
             });
         await em.persistAndFlush(user);
         return user;
+    }
+
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg('options') options: UsernamePasswordInput,
+        @Ctx() {em}: MyContext
+    ): Promise<UserResponse>{
+        const user = await em.findOne(User, {username: options.username.toLowerCase()});
+        if (!user){
+            return {
+                errors:[{
+                    field: 'username',
+                    message: "that username doesn't exist"
+                }]
+            }
+        }
+        const valid = await argon2.verify(user.password, options.password)
+        if (!valid){
+            return {
+                errors:[{
+                    field: 'password',
+                    message: "incorrect password" //Could also always return "invalid login" if you dont want to tell them what they did wrong.
+                }]
+            }
+        }
+        return {
+            user
+        };
     }
 }
